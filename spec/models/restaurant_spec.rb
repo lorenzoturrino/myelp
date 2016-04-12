7e65b448778b4d82d84626311a7b23404174c763
Hanif Fakira <fakira.work@gmail.com>
2016-04-12 17:16:03 +0100

    belongs_to association, destroy reviews if restaurant is deleted

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,5 +5,6 @@ require 'rails_helper'
 # end
 
 describe Restaurant, type: :model do
-  it { is_expected.to have_many :reviews }
+  it { is_expected.to have_many(:reviews) }
+  it {is_expected.to have_many(:reviews).dependent(:destroy) }
 end
