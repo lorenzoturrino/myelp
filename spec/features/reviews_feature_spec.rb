733c6f607be5f2c71adb2373111678062104d3e4
Robota <lorenzo.turrino@gmail.com>
2016-04-13 12:06:05 +0100

    add destroy dependency of reviews on restaurants

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,15 +3,26 @@ require 'rails_helper'
 feature 'reviewing' do
   let!(:kfc) { Restaurant.create name: 'KFC' }
 
-  scenario 'allows users to leave a review using a form' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+  feature 'allows users to leave a review using a form' do
 
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
+    before(:each) do
+      visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in 'Thoughts', with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+    end
+
+    scenario 'inserting a review redirect to the main page and show the review' do
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('so so')
+    end
+
+    scenario 'review shows on the restaurant info page' do
+      click_link 'KFC'
+      expect(page).to have_content('so so')
+    end
+
   end
 
 end
