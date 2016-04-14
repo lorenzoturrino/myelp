3f94a61cbd39150d7d2bf2c8a8d03db6aa8d0eaa
Claudia Green <claudiagreen@Claudias-MacBook-Pro-2.local>
2016-04-14 14:18:08 +0100

    Facebook sign in implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
   end
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'restaurants#index'
+  root to: 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
